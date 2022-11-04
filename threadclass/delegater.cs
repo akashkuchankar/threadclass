0ed0a45022ae513cb3b7338a9815518723c50e6c
akashkuchankar <kuchankarakash@gmail.com>
2022-11-03 20:35:19 -0700

    thread class

--- threadclass/delegater.cs
+++ threadclass/delegater.cs
@@ -1,41 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace threadclass
-{
-    // declaration of delegate
-    public delegate int MyDelegate(int a, int b);
-    public class Calc
-    {
-        public int Add(int a, int b)
-        {
-            return a + b;
-        }
-        public int Sub(int a, int b)
-        {
-            return a - b;
-        }
+//namespace threadclass
+//{
+//    // declaration of delegate
+//    public delegate int MyDelegate(int a, int b);
+//    public class Calc
+//    {
+//        public int Add(int a, int b)
+//        {
+//            return a + b;
+//        }
+       
 
-    }
-    public class Program
-    {
+//    }
+//    public class Program
+//    {
 
-        static void Main(string[] args)
-        {
-            Calc calc = new Calc();
-            MyDelegate myDelegate = new MyDelegate(calc.Add);
-            MyDelegate myDelegate2 = new MyDelegate(calc.Sub);
-            // added the method ref
-            int sum = myDelegate.Invoke(45, 23);
-            int sub = myDelegate2.Invoke(45, 23);
-            Console.WriteLine(sum);
-            Console.WriteLine(sub);
-        }
+//        static void Main(string[] args)
+//        {
+//            Calc calc = new Calc();
+//            MyDelegate myDelegate = new MyDelegate(calc.Add);
+           
+//            // added the method ref
+//            int sum = myDelegate.Invoke(45, 23);
+            
+//            Console.WriteLine(sum);
+            
+//        }
 
 
-    }
+//    }
 
-}
+//}
